89d0f8c8e18033e6b81f1a53366d623cb594da40
jeffreya <andrew-jeffrey@live.co.uk>
2017-02-22 14:38:25 +0100

    Added better test case

--- listen/src/test/java/com/javabeast/service/TeltonikaUDPToMessageServiceTest.java
+++ listen/src/test/java/com/javabeast/service/TeltonikaUDPToMessageServiceTest.java
@@ -2,10 +2,14 @@ package com.javabeast.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javabeast.teltonikia.*;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -19,13 +23,16 @@ public class TeltonikaUDPToMessageServiceTest {
     private final TeltonikaUDPToMessageService teltonikaUDPToMessageService = new TeltonikaUDPToMessageService();
     //  private final String dataString = "0178CAFE01DD31323334353637383930313233343536080400000113fc208dff000f14f650209cca80006f00d60400040004030101150316030001460000015d0000000113fc17610b000f14ffe0209cc580006e00c00500010004030101150316010001460000015e0000000113fc284945000f150f00209cd200009501080400000004030101150016030001460000015d0000000113fc267c5b000f150a50209cccc0009300680400000004030101150016030001460000015b0004";
     // private final byte[] data = dataString.getBytes();
-    private static final String MESSAGE_FILE_PATH = "/Users/jeffreya/workspace/trackr/backend/listen/src/test/resources/tracker_message.dat";
+
+    private static final URL MESSAGE_FILE_PATH = TeltonikaUDPToMessageServiceTest.class.getResource("tracker_message.dat");
     private static byte[] data;
 
-    @BeforeClass
-    public static void init() {
+    @Before
+    public void before() {
         try {
-            final Path path = Paths.get(MESSAGE_FILE_PATH);
+            final ClassLoader classLoader = getClass().getClassLoader();
+            final File file = new File(classLoader.getResource("tracker_message.dat").getFile());
+            final Path path = Paths.get(file.toURI());
             data = Files.readAllBytes(path);
         } catch (IOException e) {
             e.printStackTrace();
@@ -39,6 +46,7 @@ public class TeltonikaUDPToMessageServiceTest {
         assertNotNull(teltonikaMessage.getUdpChannelHeader());
         assertNotNull(teltonikaMessage.getAvlPacketHeader());
         assertNotNull(teltonikaMessage.getAvlData());
+
 
         final ObjectMapper objectMapper = new ObjectMapper();
         final String s = objectMapper.writeValueAsString(teltonikaMessage);
