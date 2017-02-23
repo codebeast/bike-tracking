4ae9faf423ce52e1b1f2e8806fd049c09013b09b
jeffreya <andrew-jeffrey@live.co.uk>
2017-02-23 22:22:02 +0100

    Added sprite map Added flag mappings Added th:classappend to show correct flag on selection Added static method for flag calls

--- frontend/src/main/java/com/javabeast/domain/Language.java
+++ frontend/src/main/java/com/javabeast/domain/Language.java
@@ -3,27 +3,29 @@ package com.javabeast.domain;
 import java.net.URL;
 
 public enum Language {
-    EN("en", "English", "landingpage_en.json"),
-    DE("de", "Deutsche", "landingpage_de.json"),
-    ES("es", "Español", "landingpage_es.json"),
-    IT("it", "Italiano", "landingpage_it.json"),
-    NL("nl", "Nederlands", "landingpage_nl.json"),
-    PL("pl", "Polskie", "landingpage_pl.json"),
-    PT("pt", "Português", "landingpage_pt.json"),
-    DA("da", "Dansk", "landingpage_da.json"),
-    RU("ru", "русский", "landingpage_ru.json"),
-    UK("uk", "український", "landingpage_uk.json"),
-    NO("no", "Norsk", "landingpage_no.json"),
-    SV("sv", "Svenska", "landingpage_sv.json");
+    EN("en", "English", "landingpage_en.json", "gb"),
+    DE("de", "Deutsch", "landingpage_de.json", "de"),
+    ES("es", "Español", "landingpage_es.json", "es"),
+    IT("it", "Italiano", "landingpage_it.json", "it"),
+    NL("nl", "Nederlands", "landingpage_nl.json", "nl"),
+    PL("pl", "Polskie", "landingpage_pl.json", "pl"),
+    PT("pt", "Português", "landingpage_pt.json", "pt"),
+    DA("da", "Dansk", "landingpage_da.json", "dk"),
+    RU("ru", "Pусский", "landingpage_ru.json", "ru"),
+    UK("uk", "Yкраїнський", "landingpage_uk.json", "ua"),
+    NO("no", "Norsk", "landingpage_no.json", "no"),
+    SV("sv", "Svenska", "landingpage_sv.json", "se");
 
     private final String displayName;
     private final String languageName;
     private final String landingPageUrl;
+    private final String flagClass;
 
-    Language(String displayName, String languageName, String landingPageUrl) {
+    Language(final String displayName, final String languageName, final String landingPageUrl, final String flagShortCode) {
         this.displayName = displayName;
         this.languageName = languageName;
         this.landingPageUrl = "/landingpage/" + landingPageUrl;
+        this.flagClass = "flag-" + flagShortCode;
     }
 
     public String getDisplayName() {
@@ -38,6 +40,10 @@ public enum Language {
         return landingPageUrl;
     }
 
+    public String getFlagClass() {
+        return flagClass;
+    }
+
     public static String getNameFromCode(final String langCode) {
         final String isoCode = langCode.substring(0, 2);
         final Language[] values = Language.values();
@@ -47,5 +53,16 @@ public enum Language {
             }
         }
         return Language.EN.getLanguageName();
+    }
+
+    public static String getFlagClassFromCode(final String langCode) {
+        final String isoCode = langCode.substring(0, 2);
+        final Language[] values = Language.values();
+        for (final Language value : values) {
+            if (value.getDisplayName().equals(isoCode)) {
+                return value.getFlagClass();
+            }
+        }
+        return Language.EN.getFlagClass();
     }
 }
